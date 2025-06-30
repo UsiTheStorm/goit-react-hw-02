da17140847df1c640ceef4f06ffcc27f28eb7a42
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-30 15:05:17 +0100

    Implement feedback component realisation

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
-function Feedback() {
+function Feedback({ feedback, total, positive }) {
   return (
-    <div className="stats-container">
-      <ul className="stats-row">
-        <li className="stats">Good: 0</li>
-        <li className="stats">Neutral: 0</li>
-        <li className="stats">Bad: 0</li>
-      </ul>
-      <ul className="stats-row">
-        <li className="stats">Total: 0</li>
-        <li className="stats">Positive:0%</li>
-      </ul>
-    </div>
+    total > 0 && (
+      <div className="stats-container">
+        <ul className="stats-row">
+          {Object.keys(feedback).map((fbItem) => (
+            <li key={fbItem} className="stats">
+              {fbItem.charAt(0).toUpperCase() + fbItem.slice(1)}: {feedback[fbItem]}
+            </li>
+          ))}
+        </ul>
+        <ul className="stats-row">
+          <li className="stats">Total: {total}</li>
+          <li className="stats">Positive: {positive}%</li>
+        </ul>
+      </div>
+    )
   );
 }
 
