9e234115f429805116be61c3fb76fa36682e3edc
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-30 15:05:29 +0100

    Implement options component realisation

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 
-function Options() {
+function Options({ onFeedbackUpdate, feedback, onReset, total }) {
+  console.log(feedback);
   return (
     <div className="btn-group">
-      <button className="btn feedback" id="good">
-        Good
+      {Object.keys(feedback).map((btn) => (
+        <button className="btn feedback" id={btn} key={btn} onClick={() => onFeedbackUpdate(btn)}>
+          {btn.charAt(0).toUpperCase() + btn.slice(1)}
+        </button>
+      ))}
+      <button className="btn reset" onClick={onReset} disabled={!total}>
+        Reset
       </button>
-      <button className="btn feedback" id="neutral">
-        Neutral
-      </button>
-      <button className="btn feedback" id="bad">
-        Bad
-      </button>
-      <button className="btn reset">Reset</button>
     </div>
   );
+
+  //     <div className="btn-group">
+  //       <button className="btn feedback" id="good">
+  //         Good
+  //       </button>
+  //       <button className="btn feedback" id="neutral">
+  //         Neutral
+  //       </button>
+  //       <button className="btn feedback" id="bad">
+  //         Bad
+  //       </button>
+  //       <button className="btn reset">Reset</button>
+  //     </div>
+  //   );
 }
 
 export default Options;
